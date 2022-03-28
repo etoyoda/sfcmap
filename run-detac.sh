0f5969b6a7699acd9c2b00b9a72997520897ba74
TOYODA Eizi <toyoda.eizi@gmail.com>
2022-03-28 00:19:08 +0000

    bufrconv/run-plot.sh から $refhour, $basetime を引き継ぎ動作できるように改造

--- run-detac.sh
+++ run-detac.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 set -euo pipefail
+PATH=/bin:/usr/bin
 export TZ=UTC
-today=$(date +%Y-%m-%d)
+
+: ${etcdir:='/nwp/etc'}
+
+: ${refhour:=$(date +'%Y-%m-%dT%HZ')}
+: ${basetime:=$(ruby -rtime -e 'puts(Time.at(((Time.parse(ARGV.first.sub(/Z/,":00:00Z")).to_i - 3600) / 10800) * 10800).utc.strftime("%Y-%m-%dT%H:%M:%SZ"))' $refhour)}
+: ${today:=$(ruby -rtime -e 'puts(Time.parse(ARGV.first).utc.strftime("%Y-%m-%d"))' $basetime)}
+
 datadir=/nwp/p0/${today}
 if test ! -d ${datadir} ; then
   datadir=${datadir}.new
 fi
 test -d ${datadir}
 test -f ${datadir}/obsan-${today}.tar
-ruby untar-unziplike.rb '^A_S[MI]' ${datadir}/obsan-${today}.tar > zunzip.txt
-ruby detac.rb zunzip.txt > zdetac.txt
-ruby detac-locate.rb zdetac.txt > zloctac.txt 2> zloctac.log
+ruby untar-unziplike.rb '^A_S[MI]' ${datadir}/obsan-${today}.tar > ztac.txt
+ruby detac.rb ztac.txt > zdetac.txt
+ruby detac-locate.rb -o=zloctac.txt -nsd=${etcdir}/nsd_bbsss.txt zdetac.txt 2> zloctac.log
