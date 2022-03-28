634329a48d8cf95499cd51a3183ff07f85a4d5d4
TOYODA Eizi <toyoda.eizi@gmail.com>
2022-03-28 00:16:28 +0000

    インストールスクリプト修正

--- Deploy.sh
+++ Deploy.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
+set -x
 if [ -f .deploy ] ; then
   source ./.deploy
 else
   cat > .deploy <<CONFIG
 bindir=/nwp/bin
 priv=nwp
-cgidir=/usr/lib/cgi-bin
+etcdir=/nwp/etc
 CONFIG
   echo please check config file .deploy and rerun.
   exit 1
 fi
-: ${bindir:?} ${priv:?} ${cgidir:?}
+: ${bindir:?} ${priv:?} ${etcdir:?}
 
 target="run-detac.sh detac.rb"
 
 sudo -u $priv install -m 0755 $target $bindir
+sudo -u $priv install -d $etcdir
+sudo -u $priv install -m 0644 nsd_bbsss.txt $etcdir
 
